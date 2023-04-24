3c4b28864d0ff01087410beba707d720aeeb33cf
Luis Ortiz <l.ortizintec@gmail.com>
2023-04-24 16:03:04 -0400

    lecture code along... kinda

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,28 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+    const names = ['Luis', 'Joshua', 'Andrew', 'Matt'];
+    let random = () => Math.floor(Math.random() * names.length);
+
+    const [myState, setState] = useState('initial value');
+
+    const [name, setName] = useState('Luke');
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+        setName(names[random()]);
+    }
+
     return (
         <div className="App">
-            <p>Hello! My name is Luke.</p>
-            <button>Click me!</button>
-            <p>I've clicked the button 0 times.</p>
+            <p>Hello! My name is {name}.</p>
+            <button onClick={handleClick}>
+                Click me!
+            </button>
+            <p>I've clicked the button {count} times.</p>
         </div>
     );
 }
